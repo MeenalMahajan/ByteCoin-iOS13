972423356d2de5e03781fa843b73dd816010a638
Meenal Mahajan <meenalmahajan1912@gmail.com>
2024-05-26 16:58:25 +0530

    Added coin data and coin manager

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -8,12 +8,96 @@
 
 import Foundation
 
+protocol CoinManagerDelegate{
+    
+    func didUpdateWeather(_ coinManager : CoinManager, rate : String, currency : String)
+    func didFailWithError(error : Error)
+    
+}
+
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "BA60382D-4AE2-4C97-A188-DE35C722E3AE"
+    
+    var delegate : CoinManagerDelegate?
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
+    
+    func getCoinPrice(for currency : String){
+        
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        
+        performRequest(with: urlString)
+        
+    }
 
+    
+    func performRequest(with urlString: String){
+        
+        //Step-1 Create a URL
+        if let url = URL(string: urlString){
+            
+            //Step-2 Create a URLSession
+            let session = URLSession(configuration: .default)
+            
+            //Step-3 Give URL Session a Task
+            
+            let task = session.dataTask(with: url) { data, response, error in // closure
+                
+                if error != nil{
+                    
+                    print(error!)
+                    self.delegate?.didFailWithError(error: error!)
+                    return
+                }
+                
+                if let safeData = data{
+                    
+                    let str = String.init(data: data!, encoding: .utf8)
+                  //  print(str)
+                    if let coin = self.parseJSON(safeData){
+                        
+                        
+                        let rateRound = String(format: "%.2f", coin.rate)
+                
+                        self.delegate?.didUpdateWeather(self, rate: rateRound, currency: coin.asset_id_quote)
+                    }
+                    
+                }
+                
+            }
+             
+            //Start the Task
+            task.resume()
+        }
+    }
+    
+    func parseJSON(_ data: Data) -> CoinData? {
+        
+        let decoder = JSONDecoder()
+      
+        do{
+          let decodedData =  try decoder.decode(CoinData.self, from: data)
+            
+            let lastPrice = decodedData.rate
+            let currency = decodedData.asset_id_quote
+            
+            let coindata = CoinData(rate: lastPrice, asset_id_quote: currency)
+            
+            return coindata
+            //print(weather.temperatureString)
+         
+            
+        } catch{
+           
+            print(error)
+           // delegate?.didFailWithError(error: error)
+            return nil
+        }
+        
+    
+    }
     
 }
